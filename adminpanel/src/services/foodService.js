df74033966d45d50bdec22e12fd872514d7bfdb0
dhanushya260804 <dhanushya260804@gmail.com>
2026-05-09 18:48:47 +0530

    Fixed API_URL in foodService

--- adminpanel/src/services/foodService.js
+++ adminpanel/src/services/foodService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
+import BASE_URL from '../config';
 
-const API_URL = 'BASE_URL';
+const API_URL = `${BASE_URL}/api/foods`;
 
 export const addFood = async (foodData, image) => {
     const formData = new FormData();
