97ca08b70614cd81b6a43b5ca6b94001c3472656
dhanushya260804 <dhanushya260804@gmail.com>
2026-05-23 15:33:21 +0530

    Add explicit component scan

--- src/main/java/com/fooddelivery/foodiesapi/FoodiesapiApplication.java
+++ src/main/java/com/fooddelivery/foodiesapi/FoodiesapiApplication.java
@@ -2,8 +2,10 @@ package com.fooddelivery.foodiesapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.fooddelivery.foodiesapi")
 public class FoodiesapiApplication {
 
 	public static void main(String[] args) {
